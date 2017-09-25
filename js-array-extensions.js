1df03b5a50ea58c049d30428bfa9de58a8a6cb7e
dzsoft-ru <antracid_z@mail.ru>
2017-09-25 21:31:38 +0300

    adding files

--- js-array-extensions.js
+++ js-array-extensions.js
@@ -1,11 +1,10 @@
 var arrayExt = arrayExt || {};
-
 /**
-* @method UniqueBy - Возвращает массив из уникальных занчений входящего массива
-* @param arr - Массив из объектов
-* @param fields - Свойства по которым должна проверяться уникальность
-*/
-arrayExt.UniqueBy = function (arr, fields) {
+ * @method UniqueBy - Возвращает массив из уникальных занчений входящего массива
+ * @param arr - Массив из объектов
+ * @param fields - Свойства по которым должна проверяться уникальность
+ */
+arrayExt.UniqueBy = function(arr, fields) {
     var result = [];
     if(!arr) return result;
     arr.forEach(function(item) {
@@ -31,44 +30,47 @@ arrayExt.UniqueBy = function (arr, fields) {
     return result;
 }
 /**
-* @method FilterArray - Отфильтровать массив по набору фильтров
-* @param arr - Массив из объектов
-* @param filters - Параметры для фильтрации массива (название поля, его значение)[{key:'', value:'', operator:'[defaul equal/like]'}]
-*/
-arrayExt.FilterBy = function (arr, filters) {
-	var result = [];
+ * @method FilterArray - Отфильтровать массив по набору фильтров
+ * @param arr - Массив из объектов
+ * @param filters - Параметры для фильтрации массива (название поля, его значение)[{key:'', value:'', operator:'not [equal/like]'}]
+ */
+arrayExt.FilterBy = function(arr, filters) {
+    var result = [];
     if(!arr) return result;
+    if(!filters || filters.length == 0) return arr;
     result = arr.filter(function(item) {
-        var isSuitable = true;		 
+        var isSuitable = true;
         filters.forEach(function(f) {
-			switch(f.operator)
-			{
-				case 'like':
-					if(item[f.key].indexOf(f.value)>-1) {
-						isSuitable = false;
-						return;
-					}
-				break;
-				case 'equal':
-				default:					 
-					if(item[f.key] !== f.value) {
-						isSuitable = false;
-						return;
-					}
-			}
-           
+            var invers = false;
+            var filterSuitable = false;
+            var op = f.operator
+            if(f.operator && f.operator.indexOf('not') > -1) {
+                invers = true;
+                op = f.operator.substring(4);
+            }
+            /*if(isSuitable) return true; TODO: make filters AND/OR */
+            switch(op) {
+                /*[like] always string*/
+                case 'like':
+                    if(String(item[f.key]).indexOf(f.value) > -1) filterSuitable = true;
+                    break;
+                case 'equal':
+                default:
+                    if(item[f.key] == f.value) filterSuitable = true;
+            }
+            if(invers) filterSuitable = !filterSuitable;
+            isSuitable = isSuitable & filterSuitable;
         });
         return isSuitable;
     });
     return result ? result : [];
 };
-
 /**
-* @method SortBy - Отсортировать массив по полям
-* @param arr - Массив из объектов
-* @param fields - поля по которым нужно отфильтровать ['a','b','-c']
-*/
-arrayExt.SortBy = function (arr, fields) {
+ * @method SortBy - Отсортировать массив по полям
+ * @param arr - Массив из объектов
+ * @param fields - поля по которым нужно отфильтровать ['a','b','-c']
+ */
+arrayExt.SortBy = function(arr, fields) {
     if(!arr) return arr;
     var s = '';
     fields.forEach(function(f, idx) {
