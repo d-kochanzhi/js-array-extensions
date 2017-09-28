65b293feb49842248933750e5873681d0a6cf6ae
DimK <15067042+d-kochanzhi@users.noreply.github.com>
2017-09-28 11:45:47 +0300

    Update script

--- js-array-extensions.js
+++ js-array-extensions.js
@@ -4,24 +4,24 @@ var arrayExt = arrayExt || {};
  * @param arr - Массив из объектов
  * @param fields - Свойства по которым должна проверяться уникальность
  */
-arrayExt.UniqueBy = function(arr, fields) {
+arrayExt.UniqueBy = function (arr, fields) {
     var result = [];
-    if(!arr) return result;
-    arr.forEach(function(item) {
+    if (!arr || !fields || fields.length == 0) return arr;
+    arr.forEach(function (item) {
         /*generate uniqueID*/
         var uniqueID = '';
-        fields.forEach(function(key) {
+        fields.forEach(function (key) {
             uniqueID += item[key];
         });
         /*search uniqueID in result*/
-        found = result.filter(function(i) {
+        found = result.filter(function (i) {
             return i.uniqueID == uniqueID;
         });
         /*add item to result if not exists*/
-        if(!found || found.length == 0) {
+        if (!found || found.length == 0) {
             var o = {};
             o.uniqueID = uniqueID;
-            fields.forEach(function(key) {
+            fields.forEach(function (key) {
                 o[key] = item[key];
             });
             result.push(o);
@@ -30,35 +30,34 @@ arrayExt.UniqueBy = function(arr, fields) {
     return result;
 }
 /**
- * @method FilterArray - Отфильтровать массив по набору фильтров
+ * @method FilterBy - Отфильтровать массив по набору фильтров
  * @param arr - Массив из объектов
  * @param filters - Параметры для фильтрации массива (название поля, его значение)[{key:'', value:'', operator:'not [equal/like]'}]
  */
-arrayExt.FilterBy = function(arr, filters) {
+arrayExt.FilterBy = function (arr, filters) {
     var result = [];
-    if(!arr) return result;
-    if(!filters || filters.length == 0) return arr;
-    result = arr.filter(function(item) {
+    if (!arr || !filters || filters.length == 0) return arr;
+    result = arr.filter(function (item) {
         var isSuitable = true;
-        filters.forEach(function(f) {
+        filters.forEach(function (f) {
             var invers = false;
             var filterSuitable = false;
             var op = f.operator
-            if(f.operator && f.operator.indexOf('not') > -1) {
+            if (f.operator && f.operator.indexOf('not') > -1) {
                 invers = true;
                 op = f.operator.substring(4);
             }
             /*if(isSuitable) return true; TODO: make filters AND/OR */
-            switch(op) {
+            switch (op) {
                 /*[like] always string*/
                 case 'like':
-                    if(String(item[f.key]).indexOf(f.value) > -1) filterSuitable = true;
+                    if (String(item[f.key]).indexOf(f.value) > -1) filterSuitable = true;
                     break;
                 case 'equal':
                 default:
-                    if(item[f.key] == f.value) filterSuitable = true;
+                    if (item[f.key] == f.value) filterSuitable = true;
             }
-            if(invers) filterSuitable = !filterSuitable;
+            if (invers) filterSuitable = !filterSuitable;
             isSuitable = isSuitable & filterSuitable;
         });
         return isSuitable;
@@ -70,12 +69,12 @@ arrayExt.FilterBy = function(arr, filters) {
  * @param arr - Массив из объектов
  * @param fields - поля по которым нужно отфильтровать ['a','b','-c']
  */
-arrayExt.SortBy = function(arr, fields) {
-    if(!arr) return arr;
+arrayExt.SortBy = function (arr, fields) {
+    if (!arr || !fields || fields.length == 0) return arr;
     var s = '';
-    fields.forEach(function(f, idx) {
+    fields.forEach(function (f, idx) {
         var d = 1;
-        if(f.charAt(0) == '-') {
+        if (f.charAt(0) == '-') {
             d = -1;
             f = f.slice(1);
         }
